6554cf1a25daf3b318674659af6778e48ec29f37
vikkey321 <vikkey321@gmail.com>
2017-08-30 16:58:57 +0530

    Add files via upload

---
+++ Knob_web_arduino_esp12e.ino
@@ -0,0 +1,87 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ChainableLED.h>
+#define NUM_LEDS  1
+
+
+const char* ssid = "your wifi user name";   //Replace your wifi user name
+const char* password = "your wifi password"; //Replace your wifi password
+
+ESP8266WebServer server(80);
+ChainableLED leds(12, 13, NUM_LEDS);
+
+const int led = 13;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp8266!");
+  digitalWrite(led, 0);
+}
+
+void handleNotFound(){
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  int r,g,b;
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    if(i == 0)
+      r = (server.arg(i).toInt());
+    else if(i == 1)
+      g = (server.arg(i).toInt());
+    else if(i == 2)
+      b = (server.arg(i).toInt());
+  }
+  Serial.println(r);
+  Serial.println(g);
+  Serial.println(b);
+  leds.setColorRGB(0, r, g, b);
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void){
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+}
